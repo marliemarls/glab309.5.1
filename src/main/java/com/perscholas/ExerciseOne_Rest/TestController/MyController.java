9f5a66b1d40047722f5fa4875d793f63152c10a5
Marlie Martinez <marlieahue@gmail.com>
2024-08-14 12:36:26 -0400

    first and final commit

---
+++ src/main/java/com/perscholas/ExerciseOne_Rest/TestController/MyController.java
@@ -0,0 +1,23 @@
+package com.perscholas.ExerciseOne_Rest.TestController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("home")
+public class MyController {
+    @RequestMapping({"/login"})
+    public String showlogin(){
+        return "inboxpage";
+    }
+
+    @RequestMapping("/")
+    public String simplemethod(){
+        return "inboxpage";
+    }
+    @RequestMapping("/index")
+    public String showindex(){
+        return "inboxpage";
+    }
+
+}
